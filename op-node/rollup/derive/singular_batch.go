18e75c7f75a678e5372b372d2575d4da21932c71
flywukong <2229306838@qq.com>
2025-03-24 10:03:35 +0800

    fix singularBatch

--- op-node/rollup/derive/singular_batch.go
+++ op-node/rollup/derive/singular_batch.go
@@ -72,13 +72,16 @@ func (b *SingularBatch) decode(r *bytes.Reader) error {
 }
 
 // GetSingularBatch retrieves SingularBatch from batchData
-func GetSingularBatch(batchData *BatchData) (*SingularBatch, error) {
+func GetSingularBatch(batchData *BatchData, rollupCfg *rollup.Config) (*SingularBatch, error) {
 	singularBatch, ok := batchData.inner.(*SingularBatch)
 	if !ok {
 		return nil, NewCriticalError(errors.New("failed type assertion to SingularBatch"))
 	}
 
-	//log.Info("convert singular batch timestamp", "before", singularBatch.Timestamp, "after", singularBatch.Timestamp*1000)
-	//singularBatch.Timestamp = singularBatch.Timestamp * 1000
+	if !rollupCfg.IsVolta(singularBatch.Timestamp) {
+		log.Info("convert singular batch to milliseconds timestamp", "time", singularBatch.Timestamp*1000)
+		singularBatch.Timestamp = singularBatch.Timestamp * 1000
+	}
+
 	return singularBatch, nil
 }
